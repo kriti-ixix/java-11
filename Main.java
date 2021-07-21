04c8c9faa34c15bd562473d0f3107fa287409ee2
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-07-21 13:29:33 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+String name="john";
+int age=45;
+}
+class Run{
+public static void main(String [] args){
+Main obj1=new Main();
+
+System.out.println("name=" + obj1.name);
+System.out.println("age: "+ obj1.age);
+
+}}
---
+++ Main2.java
@@ -0,0 +1,12 @@
+public class Main2{
+String name="john";
+final int age=45;
+}
+class Run{
+public static void main(String [] args){
+Main obj1=new Main();
+obj1.age=30;
+System.out.println("name=" + obj1.name);
+System.out.println("age: "+ obj1.age);
+
+}}
