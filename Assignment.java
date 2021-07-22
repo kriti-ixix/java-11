47f3d0edea416ccb40a63e0141b066c1326a97d4
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-07-22 15:19:57 +0530

    Assignment & Data Types

---
+++ Assignment.java
@@ -0,0 +1,11 @@
+class Assignment{
+int a=2; //assignment operator
+}
+class Run
+{
+public static void main(String[] args){
+Assignment ob=new Assignment();
+System.out.println(ob.a);
+int a=+3;
+System.out.println(a);
+}}
