3cbcfe1d76dc4a8fdc25514e2de4bd47bbb9fd21
Kriti <60443387+kriti-ixix@users.noreply.github.com>
2021-07-26 22:23:51 +0530

    Scanner, Strings & Switch Statements

---
+++ Scan.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+System.out.println("Enter value: ");
+String name=s.next();
+System.out.print("hello, "+name);
+}
+}
